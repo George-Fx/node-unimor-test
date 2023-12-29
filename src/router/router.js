385528d8fa544bc150a33527d539713604fb080c
George-Fx <64652126+George-Fx@users.noreply.github.com>
2023-12-29 14:07:13 +0400

    update

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/getmethod', (req, res) => {
   res.send('This is get method!');
 });
 
