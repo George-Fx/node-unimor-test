0fe36ad776a16812d212ac4bd34c27c8c04e3c39
George-Fx <64652126+George-Fx@users.noreply.github.com>
2023-12-29 14:05:15 +0400

    update

--- src/router/router.js
+++ src/router/router.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// MIDDLWARES //
-const {ifUserExists} = require('../middlewares/ifUserExists.js');
-const {hashPassword} = require('../middlewares/hashPassword.js');
-
-// CONTROLLERS //
-const {createUser} = require('../controllers/createUser.js');
-const {loginUser} = require('../controllers/loginUser.js');
-const {updateUser} = require('../controllers/updateUser.js');
-
-// ROUTES //
-router.post('/user/login', loginUser);
-router.patch('/user/update/:id', updateUser);
-router.post('/user/create', [ifUserExists, hashPassword], createUser);
+router.get('/', (req, res) => {
+  res.send('This is get method!');
+});
 
 module.exports = router;
