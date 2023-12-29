01c847cd8f458b25a3a56cfd61f55d01f367bd13
George-Fx <64652126+George-Fx@users.noreply.github.com>
2023-12-29 12:04:03 +0400

    update

--- server.js
+++ server.js
@@ -1,37 +1,17 @@
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-dotenv.config();
+const app = express();
 
-const MONGO_URI = process.env.MONGO_URI;
-const DATABASE_NAME = process.env.DATABASE_NAME;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const router = require('./src/router/router.js');
-
-mongoose.connect(MONGO_URI, {dbName: DATABASE_NAME});
-
-const DB = mongoose.connection;
-
-DB.on('error', console.error.bind(console, 'connection error:'));
-DB.once('open', () => console.log('Database connected!'));
+app.get("/", (req, res) => {
+  res.send("This is test node!");
+});
 
 const PORT = process.env.PORT || 8000;
 
-const app = express();
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-app.use(router);
-
-app.get('/', (req, res) => {
-  res.send('This is the root route!');
-});
-
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
-});
+app.listen(PORT);
 
 module.exports = app;
