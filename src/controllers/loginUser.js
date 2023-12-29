bd3dba2714505ee031be6030ce8c2c41fd1f1338
George-Fx <64652126+George-Fx@users.noreply.github.com>
2023-12-29 14:39:34 +0400

    update

--- src/controllers/loginUser.js
+++ src/controllers/loginUser.js
@@ -3,27 +3,19 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user.js'); 
 
 const loginUser = async (req, res, next) => {
-  const userName = req.body.userName;
-  res.send(`This is ${userName}`);
-  // try {
+  // const userName = req.body.userName;
+  // res.send(`This is ${userName}`);
+  try {
 
-  //   const user = await User.findOne({userName: req.body.userName});
-  //   if (!user) {
-  //     throw new Error('User not found');
-  //   }
+    const user = await User.findOne({userName: req.body.userName});
+    if (!user) {
+      res.send(`This is ${userName}`);
+    } else {
+      res.send(`This is no ${userName}`);
+    }
 
-  //   const isMatch = await bcrypt.compare(req.body.password, user.password);
-  //   if (!isMatch) {
-  //     throw new Error('Invalid password');
-  //   }
-
-  //   const token = jwt.sign({id: user._id}, 'your_jwt_secret', {expiresIn: '1h'});
-
-  //   res.status(200).json({user, token});
-  // } catch (error) {
-  //   console.error(error);
-  //   res.status(400).json({error: error.message});
-  // }
+  } catch (error) {
+  }
 };
 
 module.exports = {loginUser};
