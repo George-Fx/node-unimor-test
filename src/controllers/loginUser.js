acb9e08f1e3d04dbce25312b5624cf0f478f0501
George-Fx <64652126+George-Fx@users.noreply.github.com>
2023-12-29 14:44:25 +0400

    update

--- src/controllers/loginUser.js
+++ src/controllers/loginUser.js
@@ -3,11 +3,12 @@ const User = require('../models/user.js');
 const loginUser = async (req, res, next) => {
   try {
     const user = req.body.userName;
-    if (!user) {
-      res.send(`This is ${userName}`);
-    } else {
-      res.send(`This is no ${userName}`);
-    }
+    res.send(`This is ${userName}`);
+    // if (!user) {
+    //   res.send(`This is ${userName}`);
+    // } else {
+    //   res.send(`This is no ${userName}`);
+    // }
 
   } catch (error) {
   }
